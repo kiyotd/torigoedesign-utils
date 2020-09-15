fb2ff3ffa80b8724b2f1a00f9c9c55fa536439c7
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-09-15 12:49:38 +0900

    Rename directory --converters-> convert --validators-> validate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { isEmail } from "./validators/email";
-import { isIPv4 } from "./validators/ipv4";
-import { kebabCaseToCamelCase } from "./converters/string/kebabCaseToCamelCase";
-import { camelCaseToKebabCase } from "./converters/string/CamelCaseToKebabCase";
+import { isEmail } from "./validate/email";
+import { isIPv4 } from "./validate/ipv4";
+import { kebabCaseToCamelCase } from "./convert/string/kebabCaseToCamelCase";
+import { camelCaseToKebabCase } from "./convert/string/CamelCaseToKebabCase";
 
 export { isEmail, isIPv4, kebabCaseToCamelCase, camelCaseToKebabCase };
--- src/validate/email.ts
+++ src/validate/email.ts
@@ -1,6 +1,11 @@
-function isEmail(email: string): boolean {
+/**
+ *
+ * @param email
+ * @returns {boolean}
+ */
+const isEmail = (email: string): boolean => {
   const regEx: RegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
   return regEx.test(email);
-}
+};
 
 export { isEmail };
