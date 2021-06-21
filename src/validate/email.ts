bd6d6d257e819b0ace4124bbfe75788b91927802
kiyotd <torigoedesign+kiyotd@gmail.com>
2021-06-21 23:46:03 +0900

    refactor: regEx to regex, modify comments

--- src/validate/email.ts
+++ src/validate/email.ts
@@ -1,12 +1,12 @@
 /**
- * Is the email format correct?
+ * Is the correct email address format?
  *
  * @example isEmail("example@example.com"); // true
  * @example isEmail("example@exam@ple.com"); // false
  */
 function isEmail(email: string): boolean {
-  const regEx: RegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-  return regEx.test(email);
+  const regex: RegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
 }
 
 export { isEmail };
--- src/validate/ipv4.ts
+++ src/validate/ipv4.ts
@@ -1,12 +1,12 @@
 /**
- * Is the IPV4 address format correct?
+ * Is the correct IPV4 address format?
  *
  * @example isIPv4("255.255.255.255"); // true
  * @example isIPv4("255.255.255.256"); // false
  */
 function isIPv4(ip: string): boolean {
-  const regEx: RegExp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-  return regEx.test(ip);
+  const regex: RegExp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  return regex.test(ip);
 }
 
 export { isIPv4 };
